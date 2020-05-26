526e56a6b878f46ad7902d02e342c47e82a6311f
Luis <amail@mail.com>
2020-05-26 17:53:21 +0100

    .gitignore

--- moviesapp/models.py
+++ moviesapp/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 import datetime
 
+
 # Create your database models here
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -13,6 +14,7 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
